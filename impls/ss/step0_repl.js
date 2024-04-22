c6de09c05dc790b7df2cf550d307bb295489ec85
Sauma Saha <sauma.saha@thoughtworks.com>
2024-04-22 13:12:08 +0530

    Move the readline functions to a file

--- impls/ss/step0_repl.js
+++ impls/ss/step0_repl.js
@@ -1,7 +1,4 @@
-const readline = require("node:readline");
-const { stdin: input, stdout: output } = require("node:process");
-
-const rl = readline.createInterface({ input, output });
+const { rl } = require("./readline");
 
 const READ = (str) => str;
 const EVAL = (str) => str;
